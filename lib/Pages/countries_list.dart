05ec72bf7246b968965022346b660e59b14dab20
Josiah Tan <josiah.t.y.x@gmail.com>
2022-07-14 22:10:06 +0200

    Finished Implementation of all tasks
    TODO: Dynamic scaling for mobile

--- lib/Pages/countries_list.dart
+++ lib/Pages/countries_list.dart
@@ -14,6 +14,8 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   late List<Countries>? _countriesModel = [];
   bool isDescending = false;
+  int pageNum = 0;
+  late TextEditingController _controller;
 
   final TableRow rowSpacer = TableRow(children: [
     SizedBox(
@@ -37,11 +39,21 @@ class _MainPageState extends State<MainPage> {
   void initState() {
     super.initState();
     _getData();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<void> _getData() async {
+    _countriesModel!.clear();
+    print(_countriesModel);
     _countriesModel = (await ApiService().getCountries())!;
-    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+    setState(() {});
+    //Future.delayed(const Duration(seconds: 0)).then((value) => setState(() {}));
   }
 
   void sortAlphabetically() {
@@ -67,40 +79,91 @@ class _MainPageState extends State<MainPage> {
     setState((() => _countriesModel));
   }
 
-  Widget showList() => ListView.builder(
+  Widget potato() => DataTable(
+        columns: const <DataColumn>[
+          DataColumn(
+            label: Text(
+              'Name',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          DataColumn(
+            label: Text(
+              'Age',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+          DataColumn(
+            label: Text(
+              'Role',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ],
+        rows: const <DataRow>[
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Sarah')),
+              DataCell(Text('19')),
+              DataCell(Text('Student')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('Janine')),
+              DataCell(Text('43')),
+              DataCell(Text('Professor')),
+            ],
+          ),
+          DataRow(
+            cells: <DataCell>[
+              DataCell(Text('William')),
+              DataCell(Text('27')),
+              DataCell(Text('Associate Professor')),
+            ],
+          ),
+        ],
+      );
+
+  Widget showList(int i) => ListView.builder(
         scrollDirection: Axis.vertical,
         shrinkWrap: true,
-        // itemCount: 15,
-        itemCount: _countriesModel!.length,
+        itemCount: 10,
+        // itemCount: _countriesModel!.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Color.fromARGB(255, 204, 255, 204),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Table(
-                  // border: TableBorder.all(),
-                  columnWidths: {
-                    0: FlexColumnWidth(2),
-                    1: FlexColumnWidth(1),
-                    2: FlexColumnWidth(1),
-                    3: FlexColumnWidth(1),
-                  },
+          return SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromARGB(255, 204, 255, 204),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Table(
+                    // border: TableBorder.all(),
+                    columnWidths: {
+                      0: FlexColumnWidth(2),
+                      1: FlexColumnWidth(1),
+                      2: FlexColumnWidth(1),
+                      3: FlexColumnWidth(1),
+                    },
 
-                  children: [
-                    TableRow(
-                      children: [
-                        Text(_countriesModel![index].name.toString()),
-                        Text(_countriesModel![index].region.toString()),
-                        Text(_countriesModel![index].area.toString()),
-                        Text(_countriesModel![index].independent.toString()),
-                      ],
-                    ),
-                  ],
+                    children: [
+                      TableRow(
+                        children: [
+                          Text(_countriesModel![index + i].name.toString()),
+                          Text(_countriesModel![index + i].region.toString()),
+                          Text(_countriesModel![index + i].area.toString()),
+                          Text(_countriesModel![index + i]
+                              .independent
+                              .toString()),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -110,6 +173,8 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 227, 255, 227),
       body: SingleChildScrollView(
@@ -119,14 +184,22 @@ class _MainPageState extends State<MainPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'REIZ TECH HOMEWORK ASSIGNMENT',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 40,
-                    fontWeight: FontWeight.w900,
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    'REIZ TECH HOMEWORK ASSIGNMENT',
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
                 ),
+                // Expanded(
+                //   child: SizedBox(
+                //     width: 25,
+                //   ),
+                // ),
                 Padding(
                   padding: const EdgeInsets.only(right: 25),
                   child: ElevatedButton(
@@ -166,30 +239,7 @@ class _MainPageState extends State<MainPage> {
                           isDescending ? 'Sort Z-A' : 'Sort A-Z',
                           style: TextStyle(
                               color: Colors.black,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          elevation: 0.0,
-                          shadowColor: Colors.transparent,
-                          primary: Color.fromARGB(255, 143, 255, 143),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          )),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 25),
-                    child: ElevatedButton(
-                      onPressed: lessThanLT,
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Text(
-                          'Area: < Lithuania',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 14,
+                              //fontSize: 14,
                               fontWeight: FontWeight.bold),
                         ),
                       ),
@@ -212,7 +262,30 @@ class _MainPageState extends State<MainPage> {
                           'In Oceania',
                           style: TextStyle(
                               color: Colors.black,
-                              fontSize: 14,
+                              // fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          elevation: 0.0,
+                          shadowColor: Colors.transparent,
+                          primary: Color.fromARGB(255, 143, 255, 143),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 0),
+                    child: ElevatedButton(
+                      onPressed: lessThanLT,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          'Area: < Lithuania',
+                          style: TextStyle(
+                              color: Colors.black,
+                              //fontSize: 14,
                               fontWeight: FontWeight.bold),
                         ),
                       ),
@@ -228,7 +301,7 @@ class _MainPageState extends State<MainPage> {
                 ],
               ),
             ),
-//CountriesListHeader
+            //CountriesListHeader
             SizedBox(
               height: 10,
             ),
@@ -288,11 +361,128 @@ class _MainPageState extends State<MainPage> {
                     ? const Center(
                         child: CircularProgressIndicator(),
                       )
-                    : showList()
+                    : showList(pageNum)
               ],
-            )
+            ),
 
             //Pagination thing
+
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        pageNum = 0;
+                      });
+                    },
+                    child: Text(
+                      '1',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary: Color.fromARGB(
+                          255, 143, 255, 143), // <-- Button color
+                      onPrimary: Colors.white, // <-- Splash color
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        pageNum = 10;
+                      });
+                    },
+                    child: Text(
+                      '2',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary: Color.fromARGB(
+                          255, 143, 255, 143), // <-- Button color
+                      onPrimary: Colors.white, // <-- Splash color
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        pageNum = 20;
+                      });
+                    },
+                    child: Text(
+                      '3',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary: Color.fromARGB(
+                          255, 143, 255, 143), // <-- Button color
+                      onPrimary: Colors.white, // <-- Splash color
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        pageNum = 30;
+                      });
+                    },
+                    child: Text(
+                      '4',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary: Color.fromARGB(
+                          255, 143, 255, 143), // <-- Button color
+                      onPrimary: Colors.white, // <-- Splash color
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        pageNum = 40;
+                      });
+                    },
+                    child: Text(
+                      '5',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(20),
+                      primary: Color.fromARGB(
+                          255, 143, 255, 143), // <-- Button color
+                      onPrimary: Colors.white, // <-- Splash color
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: SizedBox(
+                      width: 20.0,
+                      child: TextField(
+                        controller: _controller,
+                        onSubmitted: (String value) async {
+                          setState(() {
+                            pageNum = (int.parse(value) - 1) * 10;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: Text('Current Page: ' + ((pageNum / 10) + 1).toString()),
+            )
           ]),
         ),
       ),
